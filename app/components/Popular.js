5fe29c09e5205e2a0cf6c188b996c07a657cd762
jane <sorkinjane@gmail.com>
2019-10-18 11:45:44 -0700

    API created

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import { fetchPopularRepos } from '../utils/api'
 
 function LanguagesNav({selected, onUpdateLanguage}) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
@@ -29,24 +30,55 @@ export default class Popular extends React.Component {
     super(props)
 
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null,
+      error: null
     }
     this.updateLanguage = this.updateLanguage.bind(this);
+    this.isLoading = this.isLoading.bind(this);
   }
   updateLanguage(selectedLanguage) {
 		this.setState({ // updated local state of the component which causes a re-render
-      selectedLanguage
+      selectedLanguage,
+      error: null,
+      repos: null,
     })
+
+    fetchPopularRepos(selectedLanguage)
+      .then((repos) => this.setState({
+        repos,
+        error: null,
+      }))
+      .catch(() => {
+        console.warn('Error fetching repos: ', error)
+
+        this.setState({
+          error: `There was an error fetching the repos`
+        })
+      })
+
+  }
+
+  isLoading() {
+    return this.state.repos === null && this.state.error === null
   }
   render() {
-		const { selectedLanguage } = this.state
+		const { selectedLanguage, repos, error } = this.state
 
 		return(
 			<React.Fragment>
 				<LanguagesNav
 					selected={selectedLanguage}
-					onUpdateLanguage={this.updateLanguage}>
-				</LanguagesNav>
+					onUpdateLanguage={this.updateLanguage}
+				/>
+
+        {this.isLoading() && <p>LOADING</p>}
+
+        {error && <p>{error}</p>}
+
+        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+
+
 			</React.Fragment>
 		)
   
