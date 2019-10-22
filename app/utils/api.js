a14751721dfd4c5ed3489480d4406c11d02f25c6
jsorkin24 <>
2019-10-22 15:45:36 -0700

    API

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,3 +1,69 @@
+function getErrorMsg(message, username) {
+  if (message === 'Not Found') {
+    return `${username} doesn't exist`
+  }
+
+  return message
+}
+
+function getProfile(username) {
+  return fetch(`https://api.github.com/users/${username}`)
+    .then((res) => res.json())
+    .then((profile) => {
+      if (profile.message) {
+        throw new Error(getErrorMsg(profile.message, username))
+      }
+      console.log('profile', profile)
+      return profile
+    })
+}
+
+function getRepos(username) {
+  return fetch(`https://api.github.com/users/${username}/repos`)
+    .then((res) => res.json())
+    .then((repos) => {
+      if (repos.message) {
+        throw new Error(getErrorMsg(repos.message, username))
+      }
+      console.log('repos', repos)
+      return repos
+
+    })
+}
+
+function getStarCount(repos) {
+  return repos.reduce((count, { stargazers_count }) => count + stargazers_count, 0)
+}
+
+function calculateScore(followers, repos) {
+  console.log('followers', followers)
+
+  return (followers * 3) + getStarCount(repos)
+
+}
+
+function getUserData(player) {
+  return Promise.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(([profile, repos]) => ({
+    profile,
+    score: calculateScore(profile.followers, repos)
+  }))
+}
+
+function sortPlayers(players) {
+  console.log('players', players)
+  return players.sort((a, b) => b.score - a.score)
+}
+
+export function battle(players) {
+  return Promise.all([
+    getUserData(players[0]),
+    getUserData(players[1])
+  ]).then((results) => sortPlayers(results))
+}
+
 export function fetchPopularRepos(language) {
   const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
 
@@ -7,7 +73,7 @@ export function fetchPopularRepos(language) {
       if (!data.items) {
         throw new Error(data.message)
       }
-
+      console.log(data.items)
       return data.items
     })
 }
